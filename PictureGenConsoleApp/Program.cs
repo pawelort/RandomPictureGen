209c9b9a3df2e7030d3856cbda662c8f0df52ef8
pawelort <pawel.ort@gmail.com>
2023-05-06 00:13:26 +0200

    Created first working version of picture generator

--- PictureGenConsoleApp/Program.cs
+++ PictureGenConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PictureGenConsoleApp.PictureGen;
+namespace PictureGenConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\dotnet projects\PictureGen\picture\new.bmp";
+            BmpImg image = new BmpImg(int.Parse(args[0]), int.Parse(args[1]));
+            image.SetRandomImg();
+            image.SaveImg(path);
+
+        }
+    }
+}
