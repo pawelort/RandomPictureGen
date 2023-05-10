dde2ac7eb5a2257a3a922fdce2bdf99a99b0f0fc
pawelort <pawel.ort@gmail.com>
2023-05-11 00:09:30 +0200

    fields with properties added to class Image

--- RandomPictureGenLib/PictureGen/Image.cs
+++ RandomPictureGenLib/PictureGen/Image.cs
@@ -5,75 +5,118 @@ namespace RandomPictureGenLib.PictureGen
 {
     public class Image
     {
-        private Bitmap image;
+        private int _width;
+        private int _height;
+        private string _path;
+
+
+        public int width
+        {
+            get { return _width; }
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    _width = 10;
+                }
+            }
+        }
+
+        public int height
+        {
+            get { return _height; }
+            set
+            {
+                if (value > 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 10;
+                }
+            }
+        }
+
+        public string path
+        {
+            get { return _path; }
+            set
+            {
+                char[] invalidPathChars = Path.GetInvalidPathChars();
+                foreach (char forbiddenSymbol in invalidPathChars)
+                {
+                    if (value.Contains(forbiddenSymbol))
+                    {
+                        _path = "";
+                        return;
+                    }
+                }
+                _path = value;
+            }
+
+        }
+    
+
+    public Image(int x, int y, string picture_path)
+    {
+        width = x;
+        height = y;
+        path = picture_path;
+    }
+
+    public Image(int x, string picture_path)
+    {
+        width = x;
+        height = x;
+        path = picture_path;
+    }
+
+    public void CreateImage()
+    {
+        Bitmap image = new Bitmap(width, height);
         Random rand = new Random();
 
-        public Image(int x, int y)
+        for (int x = 0; x < image.Width; x++)
         {
-            image = new Bitmap(x, y);
-        }
-
-        public Image(int x)
-        {
-            image = new Bitmap(x, x);
-        }
-
-        private void SetRandomBWPixel(int x, int y)
-        {
-            if (rand.Next(0, 2) % 2 == 0)
+            for (int y = 0; y < image.Height; y++)
             {
-                image.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                if (rand.Next(0, 2) % 2 == 0)
+                {
+                    image.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                }
+                else
+                {
+                    image.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+            }
+        }
+        try
+        {
+            string[] folderPath = path.Split('\\');
+            string extension = folderPath[^1];
+            DirectoryInfo dirInfo = Directory.CreateDirectory(String.Join('\\', folderPath[0..^1]));
+            if (extension.Contains("bmp"))
+            {
+                image.Save(path);
             }
             else
             {
-                image.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                image.Save(path, ImageFormat.Jpeg);
             }
         }
-
-        public void SetRandomImg()
+        catch (Exception exc)
         {
-            for (int x = 0; x < image.Width; x++)
-            {
-                for (int y = 0; y < image.Height; y++)
-                {
-                    SetRandomBWPixel(x, y);
-                }
-            }
+            Console.WriteLine($"{exc.Message}");
         }
 
-        private void SaveBmp(string path)
-        {        
-            image.Save(path);
-        }
-        private void SaveJpg(string path)
-        {
-            image.Save(path, ImageFormat.Jpeg);
-        }
-
-        public void SaveImg(string path)
-        {
-            try
-            {
-                string[] folderPath = path.Split('\\');
-                string extension = folderPath[^1];
-                DirectoryInfo dirInfo = Directory.CreateDirectory(String.Join('\\', folderPath[0..^1]));
-                if (extension.Contains("bmp"))
-                {
-                    SaveBmp(path);
-                }
-                else
-                {   
-                    SaveJpg(path);
-                }
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine($"{exc.Message}");
-            }
-
-
-        }
 
     }
+
+}
 
 }
