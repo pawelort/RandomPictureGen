94eb8c761351e0912a0683cd0267e20f193df895
pawelort <pawel.ort@gmail.com>
2023-05-17 00:53:28 +0200

    refactorisation to increase readability

--- RandomPictureGenLib/PictureGen/Image.cs
+++ RandomPictureGenLib/PictureGen/Image.cs
@@ -5,118 +5,69 @@ namespace RandomPictureGenLib.PictureGen
 {
     public class Image
     {
-        private int _width;
-        private int _height;
-        private string _path;
+        private const int DefaultWidth = 10;
+        private const int DefaultHeight = 10;
+        private readonly int width;
+        private readonly int height;
 
 
-        public int width
+
+        public int Width
         {
-            get { return _width; }
-            set
-            {
-                if (value > 0)
-                {
-                    _width = value;
-                }
-                else
-                {
-                    _width = 10;
-                }
-            }
+            get => width;
+            private init => width = value > 0 ? value : DefaultWidth;
         }
 
-        public int height
+        public int Height
         {
-            get { return _height; }
-            set
-            {
-                if (value > 0)
-                {
-                    _height = value;
-                }
-                else
-                {
-                    _height = 10;
-                }
-            }
+            get => height;
+            private init => height = value > 0 ? value : DefaultHeight;
         }
 
-        public string path
+        public static Image CreateRectangle(int x, int y)
         {
-            get { return _path; }
-            set
+            return new Image(x, y);
+        }
+
+        public static Image CreateSquare(int x)
+        {
+            return new Image(x, x);
+        }
+
+        public Image(int x, int y)
+        {
+            Width = x;
+            height = y;
+        }
+
+        public Bitmap CreateImage()
+        {
+            Bitmap image = new Bitmap(width, height);
+            Random rand = new Random();
+
+            for (int x = 0; x < image.Width; x++)
             {
-                char[] invalidPathChars = Path.GetInvalidPathChars();
-                foreach (char forbiddenSymbol in invalidPathChars)
+                for (int y = 0; y < image.Height; y++)
                 {
-                    if (value.Contains(forbiddenSymbol))
+                    if (rand.Next(0, 2) % 2 == 0)
                     {
-                        _path = "";
-                        return;
+                        image.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    }
+                    else
+                    {
+                        image.SetPixel(x, y, Color.FromArgb(255, 255, 255));
                     }
                 }
-                _path = value;
             }
 
+            return image;
         }
-    
-
-    public Image(int x, int y, string picture_path)
-    {
-        width = x;
-        height = y;
-        path = picture_path;
-    }
-
-    public Image(int x, string picture_path)
-    {
-        width = x;
-        height = x;
-        path = picture_path;
-    }
-
-    public void CreateImage()
-    {
-        Bitmap image = new Bitmap(width, height);
-        Random rand = new Random();
-
-        for (int x = 0; x < image.Width; x++)
-        {
-            for (int y = 0; y < image.Height; y++)
-            {
-                if (rand.Next(0, 2) % 2 == 0)
-                {
-                    image.SetPixel(x, y, Color.FromArgb(0, 0, 0));
-                }
-                else
-                {
-                    image.SetPixel(x, y, Color.FromArgb(255, 255, 255));
-                }
-            }
-        }
-        try
-        {
-            string[] folderPath = path.Split('\\');
-            string extension = folderPath[^1];
-            DirectoryInfo dirInfo = Directory.CreateDirectory(String.Join('\\', folderPath[0..^1]));
-            if (extension.Contains("bmp"))
-            {
-                image.Save(path);
-            }
-            else
-            {
-                image.Save(path, ImageFormat.Jpeg);
-            }
-        }
-        catch (Exception exc)
-        {
-            Console.WriteLine($"{exc.Message}");
-        }
-
 
     }
 
 }
 
-}
+
+//TODO: oddzielić zapisywanie obrazka od tworzenia obrazka
+//TODO: zrobić unit testy
+//TODO: pomyśleć nad innym sposobem zamodelowania obrazka (dwuwymiarowy array z pixelami?)
