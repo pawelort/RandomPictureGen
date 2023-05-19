edadb92f5d4728ab7bcb1e6f1e564661811f0e85
pawelort <pawel.ort@gmail.com>
2023-05-19 02:01:07 +0200

    introduced new PictureGenSettings class since Image class changed to static

---
+++ PictureGenConsoleApp/PictureSettings/PictureGenSettings.cs
@@ -0,0 +1,32 @@
+using System.Drawing;
+namespace PictureGenConsoleApp.PictureSettings
+{
+    public class PictureGenSettings
+    {
+        private const int DefaultWidth = 10;
+        private const int DefaultHeight = 10;
+        private int width;
+        private int height;
+        public int Width
+        {
+            get => width;
+            set => width = value > 0 ? value : DefaultWidth;
+        }
+
+        public int Height
+        {
+            get => height;
+            set => height = value > 0 ? value : DefaultHeight;
+        }
+
+        public PictureGenSettings(int picture_width, int picture_height)
+        {
+            width = picture_width;
+            height = picture_height;
+        }
+        public Bitmap Create()
+        {
+            return RandomPictureGenLib.PictureGen.Image.CreateRectangle(Width, Height);
+        }
+    }
+}
